adb1f1a405349f99a244221e26407df115bd8026
robson120-s <shroba391@gmail.com>
2025-09-21 18:59:26 +0300

    first commit done

---
+++ app/components/ProjectCard.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import React from 'react'
+
+const ProjectCard = () => {
+  return (
+    <button onClick={() => console.log('Project Card Clicked')}>Project </button>
+  )
+}
+
+export default ProjectCard 
---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contactpage = () => {
+  return (
+    <div>Contact me</div>
+  )
+}
+
+export default Contactpage
